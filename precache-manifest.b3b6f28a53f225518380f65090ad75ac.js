8ccd4500d6f30b1b8332f5038e93da866c97b747
Daniel Flynn <daniel.flynn@stileeducation.com>
2020-07-20 20:43:16 +1000

    Updates

---
+++ precache-manifest.b3b6f28a53f225518380f65090ad75ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95a6232c240aca0801a65c62da0aecbc",
+    "url": "/hangman-react/index.html"
+  },
+  {
+    "revision": "fa4694eb8a8fd325feec",
+    "url": "/hangman-react/static/css/main.f33bb99e.chunk.css"
+  },
+  {
+    "revision": "d339c10a80de2b7f2588",
+    "url": "/hangman-react/static/js/2.ddac53e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hangman-react/static/js/2.ddac53e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa4694eb8a8fd325feec",
+    "url": "/hangman-react/static/js/main.4f656585.chunk.js"
+  },
+  {
+    "revision": "1f1fc28a989d048782b4",
+    "url": "/hangman-react/static/js/runtime-main.30672460.js"
+  }
+]);
